353163d6c790cb254122ec1102bc483760515f0f
Jan Mewes <jan.mewes@epages.com>
2021-11-11 17:52:27 +0100

    Add real content to the callback page

--- src/views/Redirect.jsx
+++ src/views/Redirect.jsx
@@ -1,5 +1,6 @@
 import './Redirect.css';
 import { decodeState } from '../utils/state.js';
+import i18n from 'i18next';
 
 function Redirect(props) {
   
@@ -27,11 +28,46 @@ function Redirect(props) {
   
   return (
     <div className="App">
-    
-			<h1>Authentication successful</h1>
-    
-      <a href={ returnUrl }>Go back to shop</a>
-    
+
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+      		<div className="container px-5">
+      				<img src={process.env.PUBLIC_URL + '/img/Epages_Logo.png'} style={{ maxWidth: '200px' }} />
+      		</div>
+      </nav>
+
+      <header className="masthead">
+      		<div className="container px-5">
+      				<div className="row gx-5 align-items-center">
+      					<div className="col-lg-12">
+      							<div className="mb-7 mb-lg-0 text-center text-lg-start">
+      									<h1 className="display-4 lh-4 mb-6">
+                          { i18n.t('views.callback.heading.label') }
+                        </h1>
+      									<p className="lead fw-normal text-muted mb-5">
+      										{ i18n.t('views.callback.redirectionMessage.label') }
+      									</p>
+                        <p>
+                          <a href={ returnUrl }>
+                            <button type="button" className="btn btn-primary">
+                              { i18n.t('views.callback.redirectNowButton.label') }
+                            </button>
+                          </a>
+                        </p>
+      							</div>
+      					</div>
+      				</div>
+      		</div>
+      </header>
+
+      <footer className="bg-black text-center py-5">
+      	<div className="container px-5">
+      			<div className="text-white-50 small">
+      					<div className="mb-2">{ i18n.t('views.homepage.legal.copyrightNotice.label') }</div>
+      					<a href={ i18n.t('views.homepage.legal.imprintLink.label') }>{ i18n.t('views.homepage.legal.imprint.label') }</a>
+      			</div>
+      	</div>
+      </footer> 
+
     </div>
   );
 }
