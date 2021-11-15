277a5545f7d6839a20b7d7dc179864f7b27c3530
Jan Mewes <jan.mewes@epages.com>
2021-11-15 17:03:19 +0100

    Remove english locale for testing

--- src/App.jsx
+++ src/App.jsx
@@ -39,10 +39,10 @@ function App() {
   const urlParams = new URLSearchParams(window.location.search);
   
   if (locationHash === "#/terms-of-use") {
-    return(<LegalContent filename={ i18n.t('legalContent.termsOfUse') }/>);
+    return(<LegalContent filename={ i18n.t('legalContent.termsOfUse', '#legal') }/>);
   }
   if (locationHash === "#/privacy-notice") {
-    return(<LegalContent filename={ i18n.t('legalContent.privacyNotice') }/>);
+    return(<LegalContent filename={ i18n.t('legalContent.privacyNotice', '#legal') }/>);
   }
   
   if (urlParams.get("code")) {
