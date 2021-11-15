d61912477a7c99118f365fea196b53e6c2ae8ede
Jan Mewes <jan.mewes@epages.com>
2021-11-15 18:53:21 +0100

    Link logo to page start

--- src/views/Homepage.jsx
+++ src/views/Homepage.jsx
@@ -11,7 +11,9 @@ function Homepage(props) {
     
     <nav className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
         <div className="container px-5">
-            <img src={process.env.PUBLIC_URL + '/img/Epages_Logo.png'} style={{ maxWidth: '200px' }} />
+            <a href="#">
+              <img src={process.env.PUBLIC_URL + '/img/Epages_Logo.png'} style={{ maxWidth: '200px' }} />
+            </a>
           
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
