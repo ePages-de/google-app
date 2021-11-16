afd03c8668b81353b440d61e1402d677cb774b8b
Jan Mewes <jan.mewes@epages.com>
2021-11-16 10:08:45 +0100

    Used fixed footer on redirect page

--- src/views/Redirect.jsx
+++ src/views/Redirect.jsx
@@ -73,14 +73,14 @@ function Redirect(props) {
       				</div>
       		</div>
       </header>
-
+      
       <footer className="bg-black text-center py-5">
-      	<div className="container px-5">
-      			<div className="text-white-50 small">
-      					<div className="mb-2">{ i18n.t('views.homepage.legal.copyrightNotice.label') }</div>
-      					<a href={ i18n.t('views.homepage.legal.imprintLink.label') }>{ i18n.t('views.homepage.legal.imprint.label') }</a>
-      			</div>
-      	</div>
+        <div className="container px-5">
+            <div className="text-white-50 small">
+                <div className="mb-2">{ i18n.t('views.homepage.legal.copyrightNotice.label') }</div>
+                <a href={ i18n.t('views.homepage.legal.imprintLink.label') }>{ i18n.t('views.homepage.legal.imprint.label') }</a>
+            </div>
+        </div>
       </footer> 
 
     </div>
