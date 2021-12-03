64cf88a2a545e2b60eb5540f55b5353356481b8f
Jan Mewes <jan.mewes@epages.com>
2021-12-03 11:29:19 +0100

    Feature: Prevent redirect to not allow-listed domains (#5)

--- src/views/ReturnPrompt/__tests__/ReturnPrompt.spec.jsx
+++ src/views/ReturnPrompt/__tests__/ReturnPrompt.spec.jsx
@@ -20,10 +20,30 @@ it('renders token generation snippet', async () => {
   screen.getByText('Authentication successful');
 });
 
+it('prevents redirect to external uri', async () => {
+  render(<ReturnPrompt oauthResponseParams={ _oauthResponseParamsWithForbiddenExternalRedirectUri() } />)
+  
+  screen.getByText('403 Forbidden');
+});
+
+it('prevents redirect to similar external uri', async () => {
+  render(<ReturnPrompt oauthResponseParams={ _oauthResponseParamsWithForbiddedButSimilarExternalRedirectUri() } />)
+  
+  screen.getByText('403 Forbidden');
+});
+
+it('allow redirect to exactly matching external uri', async () => {
+  initializeEnglishLocale();
+  render(<ReturnPrompt oauthResponseParams={ _oauthResponseParamsWithExactlyMatchingExternalRedirectUri() } />)
+  
+  screen.getByText('Return now');
+});
+
 function _oauthResponseParamsWithState() {
   const params = new URLSearchParams();
   
-  params.set('state', 'eyJzeXN0ZW1SZWRpcmVjdFVyaSI6Imh0dHBzOi8vc2hvcHMuZXhhbXBsZS1yZXNlbGxlci5jb20iLCJvcmlnaW5hbFN0YXRlIjoiaHR0cHMlM0ElMkYlMkZteS1ncmVhdC1oYW5kaWNyYWZ0LmNvbSJ9');
+  // {"systemRedirectUri":"https://shops.reseller.epages.systems","originalState":"https://shop.example.com"}
+  params.set('state', 'eyJzeXN0ZW1SZWRpcmVjdFVyaSI6Imh0dHBzOi8vc2hvcHMucmVzZWxsZXIuZXBhZ2VzLnN5c3RlbXMiLCJvcmlnaW5hbFN0YXRlIjoiaHR0cHMlM0ElMkYlMkZzaG9wLmV4YW1wbGUuY29tIn0=');
   params.set('code', 'xxxxx');
   params.set('scope', 'email%20https://www.googleapis.com/auth/content%20https://www.googleapis.com/auth/siteverification%20https://www.googleapis.com/auth/adwords%20openid%20https://www.googleapis.com/auth/userinfo.email');
   params.set('authuser', '1');
@@ -35,6 +55,45 @@ function _oauthResponseParamsWithState() {
 function _oauthResponseParamsWithoutState() {
   const params = new URLSearchParams();
   
+  params.set('code', 'xxxxx');
+  params.set('scope', 'email%20https://www.googleapis.com/auth/content%20https://www.googleapis.com/auth/siteverification%20https://www.googleapis.com/auth/adwords%20openid%20https://www.googleapis.com/auth/userinfo.email');
+  params.set('authuser', '1');
+  params.set('prompt', 'consent');
+  
+  return params;
+}
+
+function _oauthResponseParamsWithForbiddenExternalRedirectUri() {
+  const params = new URLSearchParams();
+  
+  // {"systemRedirectUri":"https://phishing.example.com","originalState":"https://shop.example.com"}
+  params.set('state', 'eyJzeXN0ZW1SZWRpcmVjdFVyaSI6Imh0dHBzOi8vcGhpc2hpbmcuZXhhbXBsZS5jb20iLCJvcmlnaW5hbFN0YXRlIjoiaHR0cHMlM0ElMkYlMkZzaG9wLmV4YW1wbGUuY29tIn0=');
+  params.set('code', 'xxxxx');
+  params.set('scope', 'email%20https://www.googleapis.com/auth/content%20https://www.googleapis.com/auth/siteverification%20https://www.googleapis.com/auth/adwords%20openid%20https://www.googleapis.com/auth/userinfo.email');
+  params.set('authuser', '1');
+  params.set('prompt', 'consent');
+  
+  return params;
+}
+
+function _oauthResponseParamsWithForbiddedButSimilarExternalRedirectUri() {
+  const params = new URLSearchParams();
+  
+  // { systemRedirectUri: 'https://phising.fakeepages.com', originalState: 'https%3A%2F%2Fshop.example.com',}
+  params.set('state', 'eyJzeXN0ZW1SZWRpcmVjdFVyaSI6Imh0dHBzOi8vcGhpc2luZy5mYWtlZXBhZ2VzLmNvbSIsIm9yaWdpbmFsU3RhdGUiOiJodHRwcyUzQSUyRiUyRnNob3AuZXhhbXBsZS5jb20ifQ==');
+  params.set('code', 'xxxxx');
+  params.set('scope', 'email%20https://www.googleapis.com/auth/content%20https://www.googleapis.com/auth/siteverification%20https://www.googleapis.com/auth/adwords%20openid%20https://www.googleapis.com/auth/userinfo.email');
+  params.set('authuser', '1');
+  params.set('prompt', 'consent');
+  
+  return params;
+}
+
+function _oauthResponseParamsWithExactlyMatchingExternalRedirectUri() {
+  const params = new URLSearchParams();
+  
+  // { systemRedirectUri: 'https://epages.com', originalState: 'https%3A%2F%2Fshop.example.com',}
+  params.set('state', 'eyJzeXN0ZW1SZWRpcmVjdFVyaSI6Imh0dHBzOi8vZXBhZ2VzLmNvbSIsIm9yaWdpbmFsU3RhdGUiOiJodHRwcyUzQSUyRiUyRnNob3AuZXhhbXBsZS5jb20ifQ==');
   params.set('code', 'xxxxx');
   params.set('scope', 'email%20https://www.googleapis.com/auth/content%20https://www.googleapis.com/auth/siteverification%20https://www.googleapis.com/auth/adwords%20openid%20https://www.googleapis.com/auth/userinfo.email');
   params.set('authuser', '1');
